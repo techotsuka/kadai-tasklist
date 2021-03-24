e3a33c78c0365f9de033c9a7eea3a4aef8eaac14
masataka.otsuka <techotsuka501@gmail.com>
2021-03-24 11:58:51 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
     root to: 'tasks#index'
     resources :tasks
-    
-    get 'signup', to: 'users#new'
+
+#ログイン
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+#サインアップ    
+  get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :create]
  end
