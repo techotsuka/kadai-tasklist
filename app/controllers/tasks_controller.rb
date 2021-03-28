c0f01d41059afda1cba9722f3363dba3e45a6ef8
masataka.otsuka <techotsuka501@gmail.com>
2021-03-28 02:40:46 +0000

    fix2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in 
-    before_action :correct_user
+    before_action :correct_user :except[:index]
 
     def index
         @tasks = current_user.tasks.all
